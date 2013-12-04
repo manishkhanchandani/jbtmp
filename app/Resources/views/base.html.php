d058e7cd597f5d2ed5dcd5eb90719abe3648838a
swathi.paipalle@gmail.com <swathi.paipalle@gmail.com@db54c32b-475b-a1b6-1541-dd225b343e92>
2013-12-04 22:29:27 +0000

    update header

--- app/Resources/views/base.html.php
+++ app/Resources/views/base.html.php
@@ -1,6 +1,6 @@
 <!-- app/Resources/views/base.html.php -->
 <!DOCTYPE html>
-<html>
+<html ng-app>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
         <title><?php $view['slots']->output('title', 'Jobs Application') ?></title>
@@ -18,39 +18,41 @@
     </head>
     <body>
         <div class="container">
-            <header class="navbar navbar header-outer" role="banner">
-                <div class="container form-inline">
-                    <a class="logo"><img src="http://dummyimage.com/44x20/000/fff&amp;text=logo" width="160" height="100px"></a>
-                     <div class="pull-right padding-top">
-                          <div>Employers: <a>Post Jobs</a> | <a>Search Resumes</a></div>
-                          <div>Job Seekers: <a>Post Resume</a> | <a>Find Jobs</a></div>
-                    </div>
-                </div>
-            </header>
+            <?php $view->extend('::header.html.php') ?>
 
-            <nav class="navbar navbar-default" role="navigation">
-                <!-- Brand and toggle get grouped for better mobile display -->
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                        <span class="sr-only">Toggle navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-                    <a class="navbar-brand" href="#">Nav Place Holder</a>
-                </div>
+            <!--<header class="navbar navbar header-outer" role="banner">-->
+                <!--<div class="container form-inline">-->
+                    <!--<a class="logo"><img src="http://dummyimage.com/44x20/000/fff&amp;text=logo" width="160" height="100px"></a>-->
+                     <!--<div class="pull-right padding-top">-->
+                          <!--<div>Employers: <a>Post Jobs</a> | <a>Search Resumes</a></div>-->
+                          <!--<div>Job Seekers: <a>Post Resume</a> | <a>Find Jobs</a></div>-->
+                    <!--</div>-->
+                <!--</div>-->
+            <!--</header>-->
 
-                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                    <ul class="nav navbar-nav">
-                        <li class="active"><a href="#">Home</a></li>
-                        <li><a href="#">About</a></li>
-                    </ul>
+            <!--<nav class="navbar navbar-default" role="navigation">-->
+                <!--&lt;!&ndash; Brand and toggle get grouped for better mobile display &ndash;&gt;-->
+                <!--<div class="navbar-header">-->
+                    <!--<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">-->
+                        <!--<span class="sr-only">Toggle navigation</span>-->
+                        <!--<span class="icon-bar"></span>-->
+                        <!--<span class="icon-bar"></span>-->
+                        <!--<span class="icon-bar"></span>-->
+                    <!--</button>-->
+                    <!--<a class="navbar-brand" href="#">Nav Place Holder</a>-->
+                <!--</div>-->
 
-                    <ul class="nav navbar-nav navbar-right">
-                        <li><a href="#">Contact</a></li>
-                    </ul>
-                </div><!-- /.navbar-collapse -->
-            </nav>
+                <!--<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">-->
+                    <!--<ul class="nav navbar-nav">-->
+                        <!--<li class="active"><a href="#">Home</a></li>-->
+                        <!--<li><a href="#">About</a></li>-->
+                    <!--</ul>-->
+
+                    <!--<ul class="nav navbar-nav navbar-right">-->
+                        <!--<li><a href="#">Contact</a></li>-->
+                    <!--</ul>-->
+                <!--</div>&lt;!&ndash; /.navbar-collapse &ndash;&gt;-->
+            <!--</nav>-->
 
           <div class="starter-template" style="margin-top: 65px;margin-bottom: 40px; min-height: 450px;">
             <?php $view['slots']->output('_content') ?>
