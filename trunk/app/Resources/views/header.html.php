a2f678e4410f92982ddfcf31b818c36f6964a9a3
swathi.paipalle@gmail.com <swathi.paipalle@gmail.com@db54c32b-475b-a1b6-1541-dd225b343e92>
2013-12-04 22:53:23 +0000

    header

---
+++ trunk/app/Resources/views/header.html.php
@@ -0,0 +1,33 @@
+<header class="navbar navbar header-outer" role="banner">
+    <div class="container form-inline">
+        <a class="logo"><img src="http://dummyimage.com/44x20/000/fff&amp;text=logo" width="160" height="100px"></a>
+        <div class="pull-right padding-top">
+            <div>Employers: <a>Post Jobs</a> | <a>Search Resumes</a></div>
+            <div>Job Seekers: <a>Post Resume</a> | <a>Find Jobs</a></div>
+        </div>
+    </div>
+</header>
+
+<nav class="navbar navbar-default" role="navigation">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="#">Nav Place Holder</a>
+    </div>
+
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+        <ul class="nav navbar-nav">
+            <li class="active"><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+        </ul>
+
+        <ul class="nav navbar-nav navbar-right">
+            <li><a href="#">Contact</a></li>
+        </ul>
+    </div><!-- /.navbar-collapse -->
+</nav>
