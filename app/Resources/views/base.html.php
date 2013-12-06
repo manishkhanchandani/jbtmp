54d86fb1ebddab6f8686999704fb12bdc6cb7095
swathi.paipalle@gmail.com <swathi.paipalle@gmail.com@db54c32b-475b-a1b6-1541-dd225b343e92>
2013-12-06 02:18:31 +0000

    Registraion Page Changes

--- app/Resources/views/base.html.php
+++ app/Resources/views/base.html.php
@@ -1,32 +1,42 @@
-<!-- app/Resources/views/base.html.php -->
-<!DOCTYPE html>
-<html ng-app>
+<!doctype html>
+<html>
     <head>
+
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
         <title><?php $view['slots']->output('title', 'Jobs Application') ?></title>
 
-        <!-- Latest compiled and minified CSS -->
+        <!--Latest compiled and minified bootstrap CSS -->
         <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.2/css/bootstrap.min.css">
-        <!-- Optional theme -->
-        <!--<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.2/css/bootstrap-theme.min.css">
+
+        <!--Custom css-->
         <link href="<?php echo $view['assets']->getUrl('css/style.css') ?>" rel="stylesheet" type="text/css" />
-        
+
         <!-- LOAD JQUERY -->
         <script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
-        <!-- LOAD ANGULAR -->
-        <script src="//ajax.googleapis.com/ajax/libs/angularjs/1.2.0/angular.min.js"></script>
-        <!-- Latest compiled and minified JavaScript -->
+        <!-- LOAD Angular -->
+        <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/angularjs/1.2.3/angular.min.js"></script>
+        <!--Latest compiled and minified JavaScript -->
         <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.2/js/bootstrap.min.js"></script>
+
+        <!--custom script-->
+         <script>
+        var globals = {
+            'host_url': '<?php echo $view->container->parameters['host_url']; ?>',
+            'base_path': '<?php echo $view->container->parameters['base_path']; ?>',
+            'path': '<?php echo $view->container->parameters['host_url'].$view->container->parameters['base_path']; ?>'
+        };
+        </script>
+        <script src="<?php echo $view['assets']->getUrl('scripts/app.js') ?>"></script>
     </head>
     <body>
         <div class="container">
-            <?php  echo $view->render('::header.html.php') ?>
+            <?php echo $view->render('::header.html.php') ?>
 
             <div class="starter-template" style="margin-top: 65px;margin-bottom: 40px; min-height: 450px;">
                 <?php $view['slots']->output('_content') ?>
             </div>
 
-            <?php  echo $view->render('::footer.html.php') ?>
+            <?php echo $view->render('::footer.html.php') ?>
         </div>
     </body>
 </html>
--- src/Jobs/WebBundle/Resources/views/Auth/login.html.php
+++ src/Jobs/WebBundle/Resources/views/Auth/login.html.php
@@ -1,4 +1,36 @@
-<!-- src/Jobs/WebBundle/Resources/views/Auth/login.html.php -->
+
 <?php $view->extend('JobsWebBundle::layout.html.php') ?>
 
-<h1>Login</h1>
+<script src="<?php echo $view['assets']->getUrl('scripts/loginApp.js') ?>"></script>
+    
+<div class="container">
+  <div class="row">
+    <div class="col-xs-12 col-sm-6 col-sm-offset-3">
+      <h1>Simple Login Form</h1>
+      <form name="loginForm" novalidate 
+        ng-app="LoginApp" ng-controller="LoginController" 
+        rc-submit="login()">
+        <div class="form-group"
+          ng-class="{'has-error': rc.loginForm.needsAttention(loginForm.username)}">
+         <input class="form-control" name="username" type="text" 
+          placeholder="Username" required ng-model="session.username" />
+         <span class="help-block" 
+          ng-show="loginForm.username.$error.required">Required</span>
+        </div>
+        <div class="form-group"
+          ng-class="{'has-error': rc.loginForm.needsAttention(loginForm.password)}">
+          <input class="form-control" name="password" type="password" 
+            placeholder="Password" required ng-model="session.password" />
+          <span class="help-block" 
+            ng-show="loginForm.password.$error.required">Required</span>
+        </div>
+        <div class="form-group">
+          <button type="submit" class="btn btn-primary pull-right" 
+            value="Login" title="Login">
+            <span>Login</span>
+          </button>
+        </div>
+      </form>
+    </div>
+  </div>
+</div>
