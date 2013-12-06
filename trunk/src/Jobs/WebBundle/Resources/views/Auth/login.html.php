31c43a2d0f76f24483a263997d53948399519f29
swathi.paipalle@gmail.com <swathi.paipalle@gmail.com@db54c32b-475b-a1b6-1541-dd225b343e92>
2013-12-06 08:39:41 +0000

    Login Page Changes

--- trunk/src/Jobs/WebBundle/Resources/views/Auth/login.html.php
+++ trunk/src/Jobs/WebBundle/Resources/views/Auth/login.html.php
@@ -5,24 +5,25 @@
 
 <div class="container">
     <div class="row">
-        <div class="col-xs-12 col-sm-6 col-sm-offset-3">
-            <h1>Login</h1>
+        <h6 class="col-sm-4 col-sm-offset-3 signin_title">Registered users: Sign in here</h6>
+        <div class="col-xs-12 col-sm-4 col-sm-offset-3">
             <form name="loginForm" novalidate ng-app="LoginApp" ng-controller="LoginController" rc-submit="login()">
+                <small>The feature you requested is only avalable to members.Please sign in to continue...</small><br>
                 <div class="form-group" ng-class="{'has-error': (loginForm.username | shouldDisplayError:loginForm)}">
-                    <input class="form-control" name="username" type="text" placeholder="Username" required ng-model="session.username" />
-                    <span class="help-block" ng-show="loginForm.username.$error.required">Required</span>
+                    <input class="form-control" name="username" type="text" placeholder="Email Address" required ng-model="session.username" />
+<!--                    <span class="help-block" ng-show="loginForm.username.$error.required">Required</span>-->
                 </div>
                 <div class="form-group" ng-class="{'has-error': (loginForm.password | shouldDisplayError:loginForm)}">
                     <input class="form-control" name="password" type="password" placeholder="Password" required ng-model="session.password" />
-                    <span class="help-block" ng-show="loginForm.password.$error.required">Required</span>
+<!--                    <span class="help-block" ng-show="loginForm.password.$error.required">Required</span>-->
                 </div>
-                <div><a href="forgotPassword">Forgot Password</a></div>
                 <div class="form-group">
-                    <button type="submit" class="btn btn-primary pull-right" 
-                            value="Login" title="Login">
-                        <span>Login</span>
+                    <button type="submit" class="btn btn-primary" 
+                            value="Login" title="Signin">
+                        <span>Signin</span>
                     </button>
                 </div>
+                <div><a href="forgotPassword">Forgot Password?</a></div>
                 <div class="response_messages" ng-show="message">{{ message }}</div>
             </form>
         </div>
