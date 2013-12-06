a8040bcbdf7c326a97f0ae266c49d247a9dbc2c7
swathi.paipalle@gmail.com <swathi.paipalle@gmail.com@db54c32b-475b-a1b6-1541-dd225b343e92>
2013-12-06 21:04:17 +0000

    Login Page Updated

--- src/Jobs/WebBundle/Resources/views/Auth/login.html.php
+++ src/Jobs/WebBundle/Resources/views/Auth/login.html.php
@@ -8,7 +8,7 @@
         <h6 class="col-sm-4 col-sm-offset-3 signin_title">Registered users: Sign in here</h6>
         <div class="col-xs-12 col-sm-4 col-sm-offset-3">
             <form name="loginForm" novalidate ng-app="LoginApp" ng-controller="LoginController" rc-submit="login()">
-                <small>The feature you requested is only avalable to members.Please sign in to continue...</small><br>
+                <small>The feature you requested is only avalable to members.Please sign in to continue...</small><br><br>
                 <div class="form-group" ng-class="{'has-error': (loginForm.username | shouldDisplayError:loginForm)}">
                     <input class="form-control" name="username" type="text" placeholder="Email Address" required ng-model="session.username" />
 <!--                    <span class="help-block" ng-show="loginForm.username.$error.required">Required</span>-->
