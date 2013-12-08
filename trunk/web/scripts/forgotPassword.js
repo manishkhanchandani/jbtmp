cf8c26dc0efc66dec9aac3b198d150cdf3b521b8
haritha.patur@gmail.com <haritha.patur@gmail.com@db54c32b-475b-a1b6-1541-dd225b343e92>
2013-12-08 20:23:24 +0000

    created forgotPassword.js

---
+++ trunk/web/scripts/forgotPassword.js
@@ -0,0 +1,26 @@
+var appModule = angular.module('App', []);
+
+appModule.controller('Controller', function($scope, $http){
+    $scope.user = {};
+
+    $scope.submit = function() {
+        $http({
+            method: 'POST',
+            url: globals.path + 'api/user/forgot',
+            data: $.param($scope.user), /* pass in data as strings*/
+            headers: {'Content-Type': 'application/x-www-form-urlencoded'}  /* set the headers so angular passing info as form data (not request payload)*/
+        })
+        .success(function(data) {
+            console.log(data);
+
+            if (!data.success) {
+                // if not successful, display an error message
+                $scope.message = "Invalid e-mail address.";
+            } else {
+                // if successful, display the following message
+                $scope.message = "Your password has been sent to your e-mail address.";
+                //alert("Your password has been sent to your e-mail.")
+            }
+        });
+    }
+});
