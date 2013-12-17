4d21ab94360402d7e62b3a09f17fce8e0617cbda
swathi.paipalle@gmail.com <swathi.paipalle@gmail.com@db54c32b-475b-a1b6-1541-dd225b343e92>
2013-12-17 01:25:30 +0000

    Updated login page

--- web/scripts/loginApp.js
+++ web/scripts/loginApp.js
@@ -1,79 +1,37 @@
-// define custom submit directive
-var rcSubmitDirective = {
-    'rcSubmit': ['$http', function ($http) {
-        return {
-            restrict: 'A',
-            link: function(scope, formElement, attributes) {
 
-                formElement.bind('submit', function () {
-                    scope.$apply(function() {
-                        //we try to submit the form
-                        //setting 'attemped' to true to make the errors displayable
-                        scope[attributes.name].attempted = true;
-                    });
-                    // if form is not valid cancel it.
-                    if (!scope[attributes.name].$valid) {
-                        return false;
-                    }
+//"use strict";
 
-                    //form valid, final submit
-                    //console.log('valid');
-                    scope.$apply(function() {
-                        $http({
-                            method: 'POST',
-                            url: globals.path + 'api/user/login',
-                            data: $.param(scope.session), // pass in data as strings
-                            headers: {'Content-Type': 'application/x-www-form-urlencoded'}  // set the headers so angular passing info as form data (not request payload)
-                        })
-                        .success(function(data) {
-                            //console.log(data);
-
-                            if (!data.success) {
-                                // if not successful, bind errors to error variables
-                                scope.message = data.message;
-                            } else {
-                                // if successful, bind success message to message
-                                scope.message = data.message;
-                                if (data.redirectUrl) {
-                                    window.location.href = data.redirectUrl;
-                                }
-                            }
-                        });
-                    });
-                });
-            }
-        };
-    }]
-};
-
-var shouldDisplayErrorFilter = function(){
-    return function(formField, form) {
-      if(!form.attempted){
-        form.attempted = false;
-      }
-      return formField.$invalid && (formField.$dirty || form.attempted);
-    };
-};
+/* Declare app level module which depends on filters, and services */
+var loginModule = angular.module('loginApp', []);
 
 
-
-// define controller for login
-var LoginController = ['$scope',
-function ($scope) {
- 
+/* Controllers */
+loginModule.controller('LoginController', function($scope, $http) {
+    
     $scope.session = {};
- 
-    $scope.login = function () {
-        // process $scope.session
-        alert('logged in!');
+    $scope.login = function() {
+        //$scope.session.email = 'swathi.paipalle@yahoo.com'
+
+        $http({
+            method: 'POST',
+            url: globals.path + 'api/user/login',
+            data: $.param($scope.session), // pass in data as strings
+            headers: {'Content-Type': 'application/x-www-form-urlencoded'}  // set the headers so angular passing info as form data (not request payload)
+        })
+                .success(function(data) {
+                    //console.log(data);
+
+                    if (!data.success) {
+                        // if not successful, bind errors to error variables
+                        $scope.message = data.message;
+                    } else {
+                        // if successful, bind success message to message
+                        $scope.message = data.message;
+                        if (data.redirectUrl) {
+                            window.location.href = data.redirectUrl;
+                        }
+                    }
+                });
     };
-}];
 
-
-// create a module to make it easier to include in the app module
-angular.module('rcForm', [])
-.directive(rcSubmitDirective)
-.filter('shouldDisplayError', shouldDisplayErrorFilter);
-
-// define module for app
-angular.module('LoginApp', ['rcForm']);
+});
