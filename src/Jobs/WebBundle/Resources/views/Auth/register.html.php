597fbeb6bde0d3f39ffb421b73d270cc69becd23
swathi.paipalle@gmail.com <swathi.paipalle@gmail.com@db54c32b-475b-a1b6-1541-dd225b343e92>
2013-12-19 21:57:27 +0000

    Updated Employer Post Page

--- src/Jobs/WebBundle/Resources/views/Auth/register.html.php
+++ src/Jobs/WebBundle/Resources/views/Auth/register.html.php
@@ -4,62 +4,111 @@
 <h2>New Users: Sign up now</h2>
 
 <div  ng-app="App" ng-controller="Controller">
-    <form novalidate class="form-horizontal" ng-submit="submit()" name="form">
+    <form novalidate class="form-horizontal" ng-submit="submit()" name="registerForm">
         <h3>workOnOpt Registration</h3><hr>
          <div class="form-group">
              <label class="col-md-3">I will be using workOnOpt as </label>
             <div class="col-md-7">
-                  <input class="btn-group" type="radio" ng-model="user.user_type" value="1" name="user_type" required> a job seeker
-                  <input class="btn-group" type="radio" ng-model="user.user_type" value="2" name="user_type" required> an employer
-                  <input class="btn-group" type="radio" ng-model="user.user_type" value="3" name="user_type" required> a staffing agency representative
+                  <input class="btn-group" type="radio" 
+                         ng-model="user.user_type" value="1" 
+                         name="user_type" required> a job seeker
+                  <input class="btn-group" type="radio" 
+                         ng-model="user.user_type" value="2" 
+                         name="user_type" required> an employer
+                  <input class="btn-group" type="radio" 
+                         ng-model="user.user_type" value="3" 
+                         name="user_type" required> a staffing agency representative
             </div>
         </div>
         
         <h3>Account Information</h3><hr>
         <div class="form-group">
             <label class="col-md-2">First name</label>
-            <div class="col-md-4"><input class="form-control" type="text" ng-model="user.firstname" name="firstname" ng-minlength=3 ng-maxlength=20 required></div>
+            <div class="col-md-4">
+                <input class="form-control" type="text" 
+                       ng-model="user.firstname" name="firstname" 
+                       ng-minlength=3 ng-maxlength=20 required>
+            </div>
         </div>
         <div class="form-group">
             <label class="col-md-2">Last name</label>
-            <div class="col-md-4"><input class="form-control" type="text" ng-model="user.lastname" name="lastname" required></div>
+            <div class="col-md-4">
+                <input class="form-control" type="text" 
+                       ng-model="user.lastname" name="lastname" required>
+            </div>
         </div>
         <div class="form-group">
             <label class="col-md-2">Email Address</label>
-            <div class="col-md-4"><input class="form-control" type="email" ng-model="user.email" name="email" required></div>
+            <div class="col-md-4">
+                <input class="form-control" type="email" 
+                       ng-model="user.email" name="email" required>
+            </div>
         </div>
         <div class="form-group">
             <label class="col-md-2">Confirm Email Address</label>
-            <div class="col-md-4"><input class="form-control" type="email" ng-model="user.confirmEmail" name="confirmEmail" required></div>
+            <div class="col-md-4">
+                <input class="form-control" type="email" 
+                       ng-model="user.confirmEmail" name="confirmEmail" required>
+            </div>
         </div>
         <div class="form-group">
             <label class="col-md-2">Create Password</label>
-            <div class="col-md-4"><input class="form-control" type="password" ng-model="user.password" name="password" required ng-minlength="6" ></div>
-            <p class="help-block" ng-show="form.password.$error.minlength || form.password.$invalid">Password must be atleast 6 characters</p>
+            <div class="col-md-4">
+                <input class="form-control" type="password" 
+                       ng-model="user.password" name="password" 
+                       required ng-minlength="6" >
+            </div>
+            <p class="help-block" ng-show="registerForm.password.$error.minlength || registerForm.password.$invalid">
+                Password must be atleast 6 characters
+            </p>
         </div>
         <div class="form-group">
             <label class="col-md-2">Confirm Password</label>
-            <div class="col-md-4"><input class="form-control" type="password" ng-model="user.confirm" name="confirm" required></div>
+            <div class="col-md-4">
+                <input class="form-control" type="password" 
+                       ng-model="user.confirm" name="confirm" required>
+            </div>
         </div>
         
         <h3>Contact Information</h3><hr>
          <div class="form-group">
             <label class="col-md-2">Phone</label>
-            <div class="col-md-4"><input class="form-control" type="number" ng-model="user.phone" name="phone"></div>
+            <div class="col-md-4">
+                <input class="form-control" type="text" 
+                       ng-model="user.phone" name="phone" 
+                       ng-required="user.user_type == 2">
+            </div>
+        </div>
+        <div class="form-group" ng-show="user.user_type == 2">
+            <label class="col-md-2">Company Name</label>
+            <div class="col-md-4">
+                <input class="form-control" type="text" 
+                       ng-model="user.company" name="company" 
+                       ng-required="user.user_type == 2">
+            </div>
         </div>
          <div class="form-group">
             <label class="col-md-2">Address</label>
-            <div class="col-md-4"><input class="form-control" type="text" ng-model="user.address" name="address"></div>
+            <div class="col-md-4">
+                <input class="form-control" type="text" 
+                       ng-model="user.address" name="address" ng-required="user.user_type == 2">
+            </div>
         </div>
          <div class="form-group">
             <label class="col-md-2">Address2</label>
-            <div class="col-md-4"><input class="form-control" type="text" ng-model="user.address2" name="address2"></div>
+            <div class="col-md-4">
+                <input class="form-control" type="text" 
+                       ng-model="user.address2" name="address2">
+            </div>
         </div>
         <div  ng-controller="countriesController">
             <div class="form-group">
-                <label class="col-md-2">country</label>
+                <label class="col-md-2">Country</label>
                 <div class="col-md-4">
-                    <select ng-model="user.country" name="country" ng-options="country.name for country in countries" ng-change="updateCountry()">
+                    <select ng-model="user.country" name="country" 
+                            ng-options="country.name for country in countries" 
+                            ng-change="updateCountry()"
+                            ng-required="user.user_type == 2">
                         <option value="">Select country</option>
                     </select>
                 </div>
@@ -67,7 +116,9 @@
             <div class="form-group">
                 <label class="col-md-2">State</label>
                 <div class="col-md-4">
-                    <select ng-model="user.state" name="state" ng-options="state.name for state in availableStates">
+                    <select ng-model="user.state" name="state" 
+                            ng-options="state.name for state in availableStates"
+                            ng-required="user.user_type == 2">
                         <option value="">Select state</option>
                     </select>
                 </div>
@@ -75,16 +126,24 @@
         </div>
         <div class="form-group">
             <label class="col-md-2">City</label>
-            <div class="col-md-4"><input class="form-control" type="text" ng-model="user.city" name="city"></div>
+            <div class="col-md-4">
+                <input class="form-control" type="text" 
+                       ng-model="user.city" name="city"
+                       ng-required="user.user_type == 2">
+            </div>
         </div>
          <div class="form-group">
             <label class="col-md-2">Zip</label>
-            <div class="col-md-4"><input class="form-control" type="password" ng-model="user.zip" name="zip"></div>
+            <div class="col-md-4">
+                <input class="form-control" type="password" 
+                       ng-model="user.zip" name="zip"
+                       ng-required="user.user_type == 2">
+            </div>
         </div>
         
         <div class="form-group">
             <div class="col-md-5">
-                <button class="btn register" ng-disabled="form.$invalid">Register Now</button>
+                <button class="btn register" ng-disabled="registerForm.$invalid">Register Now</button>
             </div>
         </div>
         <div class="response_messages" ng-show="message">{{ message }}</div>
--- web/scripts/loginApp.js
+++ web/scripts/loginApp.js
@@ -7,7 +7,7 @@ var loginModule = angular.module('loginApp', []);
 
 /* Controllers */
 loginModule.controller('LoginController', function($scope, $http) {
-    
+
     $scope.session = {};
     $scope.login = function() {
         //$scope.session.email = 'swathi.paipalle@yahoo.com'
@@ -17,21 +17,20 @@ loginModule.controller('LoginController', function($scope, $http) {
             url: globals.path + 'api/user/login',
             data: $.param($scope.session), // pass in data as strings
             headers: {'Content-Type': 'application/x-www-form-urlencoded'}  // set the headers so angular passing info as form data (not request payload)
-        })
-                .success(function(data) {
-                    //console.log(data);
+        }).success(function(data) {
+            //console.log(data);
 
-                    if (!data.success) {
-                        // if not successful, bind errors to error variables
-                        $scope.message = data.message;
-                    } else {
-                        // if successful, bind success message to message
-                        $scope.message = data.message;
-                        if (data.redirectUrl) {
-                            window.location.href = data.redirectUrl;
-                        }
-                    }
-                });
+            if (!data.success) {
+                // if not successful, bind errors to error variables
+                $scope.message = data.message;
+            } else {
+                // if successful, bind success message to message
+                $scope.message = data.message;
+                if (data.redirectUrl) {
+                    window.location.href = data.redirectUrl;
+                }
+            }
+        });
     };
 
 });
