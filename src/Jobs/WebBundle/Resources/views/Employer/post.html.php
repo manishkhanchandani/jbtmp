f6f9a8b708fc08aeb9ed0ee7a8cd8a11d3c33b42
swathi.paipalle@gmail.com <swathi.paipalle@gmail.com@db54c32b-475b-a1b6-1541-dd225b343e92>
2013-12-20 00:10:21 +0000

    Updated Employer Post Page

--- src/Jobs/WebBundle/Resources/views/Employer/post.html.php
+++ src/Jobs/WebBundle/Resources/views/Employer/post.html.php
@@ -1,149 +1,127 @@
 <!-- src/Jobs/WebBundle/Resources/views/Empoyer/post.html.php -->
 <?php $view->extend('JobsWebBundle::layout.html.php') ?>
 <script src="<?php echo $view['assets']->getUrl('scripts/postJob.js') ?>"></script>
-<h2>Job Posting</h2>
+<h2>Post Job</h2>
 
-<div  ng-app="postJob" ng-controller="PostJobController">
-    <form novalidate class="form-horizontal" ng-submit="submit()" name="jobForm">
-        <h3>Job Posting</h3><hr>
+<div ng-app="postJob" ng-controller="PostJobController">
+    <form novalidate class="form-horizontal" ng-submit="submitJob()" name="jobForm">
+        <h4>Create your job posting</h4><hr>
          <div class="form-group">
-             <label class="col-md-3">I will be using workOnOpt as </label>
+             <label class="col-md-2">Job title</label>
+            <div class="col-md-4">
+                  <input class="form-control" type="text" ng-model="job.title"
+                         name="title" required>
+            </div>
+        </div>
+        
+        <div class="form-group">
+            <label class="col-md-2">Job Number</label>
+            <div class="col-md-4">
+                <input class="form-control" type="text" 
+                       ng-model="job.number" name="number">
+            </div>
+        </div><hr>
+        <div class="form-group">
+            <label class="col-md-2">Job Position Type</label>
+            <div class="col-md-4">
+                <select class="form-control" multiple="multiple" ng-model="job.position" name="position"
+                        ng-options="job.position for job in jobPosition"></select>
+            </div>
+        </div><hr>
+        <h5>Choose your Prefer method to receive Applications.</h5>
+        <div class="form-group">
+            <div class="col-md-5 text-right">
+                <input class="btn-group" type="radio" 
+                         ng-model="job.applyEmail" value="email" 
+                         name="apply" required> Apply to Email
+                  <input class="btn-group" type="radio" 
+                         ng-model="job.applyUrl" value="url" 
+                         name="apply" required> Apply to Url
+            </div>
+        </div>
+        <div class="form-group">
+            <label class="col-md-2">Email</label>
+            <div class="col-md-4">
+                <input class="form-control" type="email" 
+                       ng-model="job.email" name="email" required>
+            </div>
+        </div>
+        <div class="form-group">
+            <label class="col-md-2">CC Email</label>
+            <div class="col-md-4">
+                <input class="form-control" type="email" 
+                       ng-model="job.CCemail" name="CCemail">
+            </div>
+        </div><hr>
+        
+        <h5>Job Location</h5><hr>
+         <div class="form-group">
+            <label class="col-md-2">Job City</label>
+            <div class="col-md-4">
+                <input class="form-control" type="text" 
+                       ng-model="job.city" name="city" required>
+            </div>
+        </div>
+        <div class="form-group">
+            <label class="col-md-2">Job State</label>
+            <div class="col-md-4">
+                <input class="form-control" type="text" 
+                       ng-model="job.state" name="state" required>
+            </div>
+        </div>
+         <div class="form-group">
+            <label class="col-md-2">Job Country</label>
+            <div class="col-md-4">
+                <input class="form-control" type="text" 
+                       ng-model="job.country" name="country" required>
+            </div>
+        </div>
+         <div class="form-group">
+            <label class="col-md-2">Job Area Code</label>
+            <div class="col-md-4">
+                <input class="form-control" type="text" 
+                       ng-model="job.areaCode" name="areaCode" required>
+            </div>
+        </div>
+        <div class="form-group">
+            <label class="col-md-2">Job Postal Code</label>
+            <div class="col-md-4">
+                <input class="form-control" type="text" 
+                       ng-model="job.postalCode" name="postalCode" required>
+            </div>
+        </div>
+        <h5>Required Skills: Enter keywords for required position</h5>
+        <div class="form-group">            
+            <div class="col-md-4">
+                <input class="form-control" type="text" 
+                       ng-model="job.skills" name="skills" required style="margin: 0px 150px;">
+            </div>
+        </div>
+        
+        <div class="form-group">
+            <label class="col-md-2">Job Description</label>
             <div class="col-md-7">
-                  <input class="btn-group" type="radio" 
-                         ng-model="user.user_type" value="1" 
-                         name="user_type" required> a job seeker
-                  <input class="btn-group" type="radio" 
-                         ng-model="user.user_type" value="2" 
-                         name="user_type" required> an employer
-                  <input class="btn-group" type="radio" 
-                         ng-model="user.user_type" value="3" 
-                         name="user_type" required> a staffing agency representative
+                <textarea class="form-control" type="text" cols="10" rows="10"
+                          ng-model="job.description" name="description" required></textarea>
             </div>
-        </div>
-        
-        <h3>Account Information</h3><hr>
-        <div class="form-group">
-            <label class="col-md-2">First name</label>
-            <div class="col-md-4">
-                <input class="form-control" type="text" 
-                       ng-model="user.firstname" name="firstname" 
-                       ng-minlength=3 ng-maxlength=20 required>
-            </div>
-        </div>
-        <div class="form-group">
-            <label class="col-md-2">Last name</label>
-            <div class="col-md-4">
-                <input class="form-control" type="text" 
-                       ng-model="user.lastname" name="lastname" required>
-            </div>
-        </div>
-        <div class="form-group">
-            <label class="col-md-2">Email Address</label>
-            <div class="col-md-4">
-                <input class="form-control" type="email" 
-                       ng-model="user.email" name="email" required>
-            </div>
-        </div>
-        <div class="form-group">
-            <label class="col-md-2">Confirm Email Address</label>
-            <div class="col-md-4">
-                <input class="form-control" type="email" 
-                       ng-model="user.confirmEmail" name="confirmEmail" required>
-            </div>
-        </div>
-        <div class="form-group">
-            <label class="col-md-2">Create Password</label>
-            <div class="col-md-4">
-                <input class="form-control" type="password" 
-                       ng-model="user.password" name="password" 
-                       required ng-minlength="6" >
-            </div>
-            <p class="help-block" ng-show="registerForm.password.$error.minlength || registerForm.password.$invalid">
-                Password must be atleast 6 characters
-            </p>
-        </div>
-        <div class="form-group">
-            <label class="col-md-2">Confirm Password</label>
-            <div class="col-md-4">
-                <input class="form-control" type="password" 
-                       ng-model="user.confirm" name="confirm" required>
-            </div>
-        </div>
-        
-        <h3>Contact Information</h3><hr>
+        </div><hr>
+        <h5>Contact Information</h5>
          <div class="form-group">
-            <label class="col-md-2">Phone</label>
             <div class="col-md-4">
-                <input class="form-control" type="text" 
-                       ng-model="user.phone" name="phone" 
-                       ng-required="user.user_type == 2">
+                <input class="" type="checkbox" ng-model="job.contact_name" name="contact_Info"><span> Name:</span>
+                <br><input class="" type="checkbox" ng-model="job.contact_address1" name="contact_Info"><span> Address Line 1:</span>
+                <br><input class="" type="checkbox" ng-model="job.contact_address2" name="contact_Info"><span> Address Line 2:</span>
+                <br><input class="" type="checkbox" ng-model="job.contact_city" name="contact_Info"><span> City:</span>
+                <br><input class="" type="checkbox" ng-model="job.contact_state" name="contact_Info"><span> State:</span>
+                <br><input class="" type="checkbox" ng-model="job.contact_zip" name="contact_Info"><span> Zip:</span>
+                <br><input class="" type="checkbox" ng-model="job.contact_phone" name="contact_Info"><span> Phone:</span>
+                <br><input class="" type="checkbox" ng-model="job.contact_email" name="contact_Info"><span> Email:</span>
             </div>
-        </div>
-        <div class="form-group" ng-show="user.user_type == 2">
-            <label class="col-md-2">Company Name</label>
-            <div class="col-md-4">
-                <input class="form-control" type="text" 
-                       ng-model="user.company" name="company" 
-                       ng-required="user.user_type == 2">
-            </div>
-        </div>
-         <div class="form-group">
-            <label class="col-md-2">Address</label>
-            <div class="col-md-4">
-                <input class="form-control" type="text" 
-                       ng-model="user.address" name="address" ng-required="user.user_type == 2">
-            </div>
-        </div>
-         <div class="form-group">
-            <label class="col-md-2">Address2</label>
-            <div class="col-md-4">
-                <input class="form-control" type="text" 
-                       ng-model="user.address2" name="address2">
-            </div>
-        </div>
-        <div  ng-controller="countriesController">
-            <div class="form-group">
-                <label class="col-md-2">Country</label>
-                <div class="col-md-4">
-                    <select ng-model="user.country" name="country" 
-                            ng-options="country.name for country in countries" 
-                            ng-change="updateCountry()"
-                            ng-required="user.user_type == 2">
-                        <option value="">Select country</option>
-                    </select>
-                </div>
-            </div>
-            <div class="form-group">
-                <label class="col-md-2">State</label>
-                <div class="col-md-4">
-                    <select ng-model="user.state" name="state" 
-                            ng-options="state.name for state in availableStates"
-                            ng-required="user.user_type == 2">
-                        <option value="">Select state</option>
-                    </select>
-                </div>
-            </div>
-        </div>
-        <div class="form-group">
-            <label class="col-md-2">City</label>
-            <div class="col-md-4">
-                <input class="form-control" type="text" 
-                       ng-model="user.city" name="city"
-                       ng-required="user.user_type == 2">
-            </div>
-        </div>
-         <div class="form-group">
-            <label class="col-md-2">Zip</label>
-            <div class="col-md-4">
-                <input class="form-control" type="password" 
-                       ng-model="user.zip" name="zip"
-                       ng-required="user.user_type == 2">
-            </div>
-        </div>
+         </div><hr>
         
         <div class="form-group">
             <div class="col-md-5">
-                <button class="btn register" ng-disabled="registerForm.$invalid">Register Now</button>
+                <button class="btn register" ng-disabled="jobForm.$invalid">Continue</button>{{jobForm.$invalid}}
             </div>
         </div>
         <div class="response_messages" ng-show="message">{{ message }}</div>
