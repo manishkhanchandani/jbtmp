90a32618fe05e04095f1082947de72d74624f57a
haritha.patur@gmail.com <haritha.patur@gmail.com@db54c32b-475b-a1b6-1541-dd225b343e92>
2013-12-27 20:03:47 +0000

    Created post resume page and post resume js file

--- src/Jobs/WebBundle/Resources/views/Jobseeker/post.html.php
+++ src/Jobs/WebBundle/Resources/views/Jobseeker/post.html.php
@@ -1,4 +1,106 @@
 <!-- src/Jobs/WebBundle/Resources/views/JobSeeker/post.html.php -->
 <?php $view->extend('JobsWebBundle::layout.html.php') ?>
+<script src="<?php echo $view['assets']->getUrl('scripts/postResume.js') ?>"></script>
+<h2>Post Your Resume</h2>
 
-Hello World - Jobseeker
+<div ng-app="postResume" ng-controller="PostResumeController">
+    <form novalidate class="form-horizontal" ng-submit="submitResume()" name="resumeForm">
+        <br>
+         <div class="form-group">
+             <label class="col-md-2">Desired Job Title</label>
+            <div class="col-md-4">
+                  <input class="form-control" type="text" ng-model="job.title"
+                         name="title" required>
+            </div>
+        </div>
+        
+        <div class="form-group">
+            <label class="col-md-2">Contact Number</label>
+            <div class="col-md-4">
+                <input class="form-control" type="text" 
+                       ng-model="resume.contactNumber" name="number">
+            </div>
+        </div>
+        
+        <div class="form-group">
+            <label class="col-md-2">Available to start</label>
+            <div class="col-md-4">
+                <input class="form-control" type="text" 
+                       ng-model="resume.startDate" name="startdate">
+            </div>
+        </div>
+        
+        <div class="form-group">
+            <label class="col-md-2">Work Authorization</label>
+            <div class="col-md-4">
+                <input class="form-control" type="text" 
+                       ng-model="resume.workAuthorization" name="workauthorization">
+            </div>
+        </div>
+        
+        <div class="form-group">
+            <label class="col-md-2">Education</label>
+            <div class="col-md-4">
+                <input class="form-control" type="text" 
+                       ng-model="resume.education" name="education">
+            </div>
+        </div>
+        
+        <div class="form-group">
+            <label class="col-md-2">School</label>
+            <div class="col-md-4">
+                <input class="form-control" type="text" 
+                       ng-model="resume.school" name="school">
+            </div>
+        </div>
+             
+        <div class="form-group">
+            <label class="col-md-2">Major</label>
+            <div class="col-md-4">
+                <input class="form-control" type="text" 
+                       ng-model="resume.major" name="major">
+            </div>
+        </div>
+        
+        <div class="form-group">
+            <label class="col-md-2">Work experience</label>
+            <div class="col-md-4">
+                <input class="form-control" type="text" 
+                       ng-model="resume.workExperience" name="experience">
+            </div>
+        </div>
+        
+        <div class="form-group">
+            <label class="col-md-2">Skill set</label>
+            <div class="col-md-4">
+                <input class="form-control" type="text" 
+                       ng-model="resume.skillSet" name="skillset">
+            </div>
+        </div>
+        
+        <div class="form-group">
+            <label class="col-md-2">Preferred location</label>
+            <div class="col-md-4">
+                <input class="form-control" type="text" 
+                       ng-model="resume.location" name="location">
+            </div>
+        </div>
+        <hr>
+        
+        <div class="form-group">
+            <div class="col-md-5">
+        <!--<button class="btn register" ng-click="uploadFile()" >Upload your resume</button>-->
+        <label for="fileToUpload">Attach your resume in MS WORD or PDF format</label><br />
+             </div>
+        </div>
+        <input class="form-control" type="file" ng-model="resume.fileupload" id="fileToUpload" multiple onchange="angular.element(this).scope().setFiles(this)" />
+        
+        <br>
+        <div class="form-group">
+            <div class="col-md-5">
+                <button class="btn register" ng-disabled="resumeForm.$invalid">Continue</button>{{resumeForm.$invalid}}
+            </div>
+        </div>
+        <div class="response_messages" ng-show="message">{{ message }}</div>
+    </form>
+</div>
