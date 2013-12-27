f29af79bf326269ffb22ead9c73ca58e2b26f065
haritha.patur@gmail.com <haritha.patur@gmail.com@db54c32b-475b-a1b6-1541-dd225b343e92>
2013-12-27 20:03:47 +0000

    Created post resume page and post resume js file

---
+++ trunk/web/scripts/postResume.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var jobPostModule = angular.module('postResume', []);
+
+jobPostModule.controller('PostResumeController', function($scope, $http) {
+
+    $scope.submitResume = function() {
+        $http({
+            method: 'POST',
+            url: globals.path + 'api/employer/post',
+            data: $.param($scope.job), /* pass in data as strings*/
+            headers: {'Content-Type': 'application/x-www-form-urlencoded'}  /* set the headers so angular passing info as form data (not request payload)*/
+        })
+        .success(function(data) {
+            console.log(data);
+
+            if (!data.success) {
+                // if not successful, bind errors to error variables
+                $scope.message = data.message;
+            } else {
+                // if successful, bind success message to message
+                $scope.message = data.message;
+                window.location.href = globals.path + 'employer/post/confirm';
+            }
+        });
+    };
+});
