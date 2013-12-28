21a522a83fda16fa64ba82ac9f0222df76dbf91c
haritha.patur@gmail.com <haritha.patur@gmail.com@db54c32b-475b-a1b6-1541-dd225b343e92>
2013-12-28 02:23:42 +0000

    created preview page

--- trunk/src/Jobs/WebBundle/Controller/JobseekerController.php
+++ trunk/src/Jobs/WebBundle/Controller/JobseekerController.php
@@ -2,6 +2,7 @@
 namespace Jobs\WebBundle\Controller;
 
 use Jobs\WebBundle\Controller\MainController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class JobseekerController extends MainController
@@ -14,5 +15,15 @@ class JobseekerController extends MainController
         }
         $this->session->remove('_redirectURL');
         return $this->render('JobsWebBundle:Jobseeker:post.html.php', array());
+    }
+    public function previewAction(Request $request)
+    {
+        if (empty($_COOKIE['accessToken'])) {
+            $this->session->set('_redirectURL', $url = $this->generateUrl('jobs_web_jobseeker_preview'));
+            return $this->redirect($this->generateUrl('jobs_web_auth_login'));
+        }
+        $id = $request->query->get('id');
+        $this->session->remove('_redirectURL');
+        return $this->render('JobsWebBundle:Jobseeker:preview.html.php', array('id' => $id));
     }
 }
