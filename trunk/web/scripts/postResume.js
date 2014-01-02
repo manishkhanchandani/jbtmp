0584468d51e8bffd76f8e789d0e0fd7f2045248a
haritha.patur@gmail.com <haritha.patur@gmail.com@db54c32b-475b-a1b6-1541-dd225b343e92>
2014-01-02 02:40:48 +0000

    updated postresume page

--- trunk/web/scripts/postResume.js
+++ trunk/web/scripts/postResume.js
@@ -1,17 +1,21 @@
 'use strict';
 
-var jobPostModule = angular.module('postResume', []);
+var resumePostModule = angular.module('postResume', ['angularFileUpload']);
 
-jobPostModule.controller('PostResumeController', function($scope, $http) {
+resumePostModule.controller('PostResumeController', function($scope, $http) {
 
+    $scope.job = {};
+    //angular.module('myApp', ['angularFileUpload']);
+       
     $scope.submitResume = function() {
+        
         $http({
             method: 'POST',
             url: globals.path + 'api/resume/post',
-            data: $.param($scope.job), /* pass in data as strings*/
+            data: $.param($scope.resume), /* pass in data as strings*/
             headers: {'Content-Type': 'application/x-www-form-urlencoded'}  /* set the headers so angular passing info as form data (not request payload)*/
         })
-        .success(function(data) {
+           .success(function(data) {
             console.log(data);
 
             if (!data.success) {
@@ -21,7 +25,24 @@ jobPostModule.controller('PostResumeController', function($scope, $http) {
                 // if successful, bind success message to message
                 $scope.message = data.message;
                 window.location.href = globals.path + 'employer/post/preview?id=1';
+                //window.location.href = globals.path + 'employer/post/confirm';
             }
         });
     };
 });
+resumePostModule.controller('MyCtrl', function($scope, $upload) {
+$scope.onFileSelect = function($files) {
+    //$files: an array of files selected, each file has name, size, and type.
+    for (var i = 0; i < $files.length; i++) {
+      var $file = $files[i];
+      $upload.upload({
+        url: 'my/upload/url',
+        file: $file,
+        progress: function(e){}
+      }).then(function(data, status, headers, config) {
+        // file is uploaded successfully
+        console.log(data);
+      }); 
+    }
+  };
+  });
