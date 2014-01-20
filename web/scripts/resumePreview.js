06b46b260a9c917105ec2f8baf719bd2398975a7
haritha.patur@gmail.com <haritha.patur@gmail.com@db54c32b-475b-a1b6-1541-dd225b343e92>
2014-01-20 05:39:00 +0000

    updated preview resume page

---
+++ web/scripts/resumePreview.js
@@ -0,0 +1,25 @@
+'use strict';
+
+var resumePreviewModule = angular.module('resumePreview', []);
+resumePreviewModule.controller('previewController', function($scope, $http) {
+    $scope.resume = {};
+    $scope.showpreview = function() {
+     return http({
+            method: 'GET',
+            url: '/api/resume/preview?id='+data.id,
+            data: $.param($scope.resume), /* pass in data as strings*/
+            headers: {'Content-Type': 'application/x-www-form-urlencoded'} /* set the headers so angular passing info as form data (not request payload)*/
+        })
+           .success(function(data) {
+            if (!data.success) {
+                // if not successful, bind errors to error variables
+                $scope.message = data.message;
+            } else {
+                // if successful, bind success message to message
+                $scope.message = data.message;
+                //window.location.href = globals.path + 'jobseeker/preview?id='+data.id;
+            }
+        });
+    };
+});
+
