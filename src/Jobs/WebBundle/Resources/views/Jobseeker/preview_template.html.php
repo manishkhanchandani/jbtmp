81a6e807f59421e283b4938ba5aa7afe341347a9
naveenkhanchandani@gmail.com <naveenkhanchandani@gmail.com@db54c32b-475b-a1b6-1541-dd225b343e92>
2014-02-02 22:20:01 +0000

    resume preview

---
+++ src/Jobs/WebBundle/Resources/views/Jobseeker/preview_template.html.php
@@ -0,0 +1,14 @@
+	<h2>Resume Preview</h2>
+    <div class="row-fluid show-grid">
+        <div class="col-md-2">Title: {{data.resumeTitle}}</div><br>
+        <div class="col-md-2">Contact Number: {{data.contactNumber}}</div><br>
+        <div class="col-md-2">Start date: {{data.startDate}}</div><br>
+        <div class="col-md-2">Work Authorization: {{data.workAuthorization}}</div><br>
+        <div class="col-md-2">Education: {{data.education}}</div><br>
+        <div class="col-md-2">Schoo: {{data.school}}</div><br>
+        <div class="col-md-2">Major: {{data.major}}</div><br>
+        <div class="col-md-2">Work Experience: {{data.workExperience}}</div><br>
+        <div class="col-md-2">Skill Set: {{data.skillSet}}</div><br>
+        <div class="col-md-2">Location: {{data.location}}</div><br><br/ >
+		<iframe id="myresumedata" ng-src="" width="800" height="600" />
+    </div>
