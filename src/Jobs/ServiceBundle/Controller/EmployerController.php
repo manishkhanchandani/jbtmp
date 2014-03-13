15e6fd69422d32c795751a3bd334029a50c1216a
naveenkhanchandani@gmail.com <naveenkhanchandani@gmail.com@db54c32b-475b-a1b6-1541-dd225b343e92>
2014-03-13 03:39:40 +0000

    status change

--- src/Jobs/ServiceBundle/Controller/EmployerController.php
+++ src/Jobs/ServiceBundle/Controller/EmployerController.php
@@ -79,6 +79,111 @@ class EmployerController extends MainController
     }
 
     
+    public function updateAction(Request $request)
+    {
+        $result = 0;
+        $msg = '';
+        $code = 200;
+        try {
+            $this->init($request);
+            $jobId = $request->query->get('jobId');
+            if (empty($jobId)) {
+                throw new \Exception('Job Id not found in url.');
+            }
+            $created = tstobts(time());
+            $em = $this->getDoctrine()->getManager();
+            $jobs = $em->getRepository('JobsServiceBundle:Jobs')->find($jobId);
+
+            if (!$jobs) {
+                throw new \Exception('No Job Found.');
+            }
+            //$jobs->setJobId($jobId);
+            $jobs->setUserId($this->userId);
+            $jobs->setTitle($request->request->get('title'));
+            $position = $request->request->get('position');
+            $position_type = $position[0]['id'];
+            $jobs->setPositionType($position_type);
+            $jobs->setApplicationMethod($request->request->get('apply'));
+            $jobs->setApplicationEmail($request->request->get('email'));
+            $jobs->setApplicationEmailCc($request->request->get('CCemail'));
+            $jobs->setApplicationUrl($request->request->get('url'));
+            $city = $request->request->get('city');
+            $cityName = isset($city['name']) ? $city['name'] : null;
+            $jobs->setCity($cityName);
+            $cityId = isset($city['id']) ? $city['id'] : null;
+            $jobs->setCityId($cityId);
+            $state = $request->request->get('state');
+            $stateId = isset($state['name']) ? $state['name'] : null;
+            $jobs->setState($stateId);
+            $country = $request->request->get('country');
+            $countryId = isset($country['name']) ? $country['name'] : null;
+            $jobs->setCountry($countryId);
+            $jobs->setAreaCode($request->request->get('areaCode'));
+            $jobs->setZipcode($request->request->get('postalCode'));
+            $jobs->setSkills($request->request->get('skills'));
+            $jobs->setDescription($request->request->get('description'));
+            $jobs->setShowName($this->setCheckbox($request->request->get('contact_name')));
+            $jobs->setShowAddress1($this->setCheckbox($request->request->get('contact_address1')));
+            $jobs->setShowAddress2($this->setCheckbox($request->request->get('contact_address2')));
+            $jobs->setShowCity($this->setCheckbox($request->request->get('contact_city')));
+            $jobs->setShowState($this->setCheckbox($request->request->get('contact_state')));
+            $jobs->setShowEmail($this->setCheckbox($request->request->get('contact_email')));
+            $jobs->setShowPhone($this->setCheckbox($request->request->get('contact_phone')));
+            $jobs->setShowZipcode($this->setCheckbox($request->request->get('contact_zip')));
+            $latitude = isset($city['latitude']) ? $city['latitude'] : null;
+            $longitude = isset($city['longitude']) ? $city['longitude'] : null;
+            $jobs->setLatitude($latitude);
+            $jobs->setLongitude($longitude);
+            //$jobs->setJobCreatedDt($created);
+            //$jobs->setJobDeleted(0);
+            //$jobs->setJobDeletedDt(0);
+            $jobs->setJobModifiedDt($created);
+                //$jobs->setJobStatus(1);
+            $em->flush();
+            $msg = 'Success';
+            $result = 1;
+        } catch (\Exception $e) {
+            $msg = $e->getMessage();
+            $result = 0;
+            $code = $e->getCode();
+        }
+        $arr = array('success' => $result, 'message' => $msg, 'code' => $code, 'post' => $_POST);
+        $json = json_encode($arr);
+        return new Response($json);
+    }
+
+    
+    public function statuschangeAction(Request $request)
+    {
+        $result = 0;
+        $msg = '';
+        $code = 200;
+        try {
+            $this->init($request);
+            $status = $request->request->get('status');
+            //$status = array('jobId' => status, 'jobid2' => status);
+            if (empty($status)) {
+                throw new \Exception('Status not found.');
+            }
+            $created = tstobts(time());
+            $em = $this->getDoctrine()->getManager();
+            foreach ($status as $jobId => $value) {
+                $jobs = $em->getRepository('JobsServiceBundle:Jobs')->find($jobId);
+                $jobs->setJobStatus($value);
+                $em->flush();
+            }
+            $msg = 'Success';
+            $result = 1;
+        } catch (\Exception $e) {
+            $msg = $e->getMessage();
+            $result = 0;
+            $code = $e->getCode();
+        }
+        $arr = array('success' => $result, 'message' => $msg, 'code' => $code, 'post' => $_POST);
+        $json = json_encode($arr);
+        return new Response($json);
+    }
+    
     public function jobsAction(Request $request)
     {
         $result = 0;
